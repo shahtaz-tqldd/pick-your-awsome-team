7c3616a8d5fc953dce793962d256357cf33d4ef5
Shahtaz Rahman <shahtazrahman17@gmail.com>
2022-08-23 00:32:13 +0600

    player adding functionality added

--- js/functionality.js
+++ js/functionality.js
@@ -1,26 +1,24 @@
-function clickedPlayerSelectButton(eventId, playername){
-    document.getElementById(eventId).addEventListener('click', function(){
-        const textElement = document.getElementById(playername)
-        const playerName = textElement.innerText
-    
-        const playerNameContainer = document.getElementById('player-name-container')
-        const li = document.createElement('li')
-        li.innerText = playerName
-        playerNameContainer.appendChild(li)
-    
-        const buttonClicked = document.getElementById(eventId)
-        buttonClicked.classList.add('disabled')
-    })
+function setTextElementById(eventId, value){
+    const textElement = document.getElementById(eventId)
+    textElement.innerText = value + 1
 }
 
-clickedPlayerSelectButton('btn-select-sakib', 'player-name-sakib')
-clickedPlayerSelectButton('btn-select-mahmudullah', 'player-name-mahmudullah')
-clickedPlayerSelectButton('btn-select-soumya', 'player-name-soumya')
-clickedPlayerSelectButton('btn-select-liton', 'player-name-liton')
-clickedPlayerSelectButton('btn-select-mash', 'player-name-mash')
-clickedPlayerSelectButton('btn-select-mosaddek', 'player-name-mosaddek')
-clickedPlayerSelectButton('btn-select-tamim', 'player-name-tamim')
-clickedPlayerSelectButton('btn-select-mushfiq', 'player-name-mushfiq')
-clickedPlayerSelectButton('btn-select-mostafiz', 'player-name-mostafiz')
+function myFunction(element) {
+    const playerName = element.parentNode.children[0].innerText
+    const buttonClicked = element
+    const playerNameContainer = document.getElementById('player-name-container')
+    
 
+    const numberOfPlayerSelected = document.querySelectorAll("#player-name-container li").length
+    if( numberOfPlayerSelected >4){
+        alert('Sorry You Can not add more than 5 Players')
+        return;
+    }
+    const li = document.createElement('li')
+    li.innerText = playerName
+    playerNameContainer.appendChild(li)
 
+    setTextElementById('player-number', numberOfPlayerSelected)
+
+    buttonClicked.classList.add('disabled')
+}
