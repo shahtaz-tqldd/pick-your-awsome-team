4ad80067635506ee9f4891d4054215f0d3b47701
Shahtaz Rahman <shahtazrahman17@gmail.com>
2022-08-22 20:35:43 +0600

    budget section done

--- js/budget.js
+++ js/budget.js
@@ -1,3 +1,4 @@
+// getting provided input field number
 function getElementInputField(eventId){
     const elementField = document.getElementById(eventId)
     const elementString = elementField.value;
@@ -6,13 +7,29 @@ function getElementInputField(eventId){
     return getInputNumber;
 }
 
+// setting calculated number into an innertext
 function setElementTextById(textId, value){
     const textElementField = document.getElementById(textId)
     textElementField.innerText = value
 }
 
+// Calculation of total budget of 5 players
 document.getElementById('btn-calculate').addEventListener('click', function(){
     const playerBudget = getElementInputField('player-budget-field')
     const totalPlayerBudget = playerBudget * 5;
     setElementTextById('total-player-budget', totalPlayerBudget)
+})
+
+// calculation of total expenses
+
+document.getElementById('btn-calculate-total').addEventListener('click', function(){
+    const managerBudget = getElementInputField('manager-budget-field')
+    const coachBudget = getElementInputField('coach-budget-field')
+    
+    const playerBudget = getElementInputField('player-budget-field')
+    const totalPlayerBudget = playerBudget * 5;
+ 
+    const totalBudget = managerBudget + coachBudget + totalPlayerBudget;
+
+    setElementTextById('total-expense', totalBudget)
 })
